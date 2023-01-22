ccf55a4d3f6b68a408e77424d29d1a8a6d8626fb
DN050498MAM <andrii.moskalenko@privatbank.ua>
2023-01-22 19:20:34 +0200

    Add fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import Game from './components/Game/Game'
 import io from 'socket.io-client';
 
-const socket = io.connect(window.location);
+const socket = io.connect();
 
 function App() {
   return <Game socket={socket}/>;
